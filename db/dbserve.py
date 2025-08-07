e03111c0769dc1f727d662150ee1fa5ef902b1a1
KajalB03 <ksharmams02@yahoo.com>
2025-08-07 17:50:29 -0600

    Update dbserve.py

--- db/dbserve.py
+++ db/dbserve.py
@@ -1,1 +1,24 @@
 
+import duckdb
+import os
+
+def main():
+    con = duckdb.connect()
+
+    # Enable DuckDB's built-in HTTP server extension
+    con.install_extension("httpserver", repository="community")
+    con.load_extension("httpserver")
+
+    # Parquet file as a table
+    con.execute("CREATE TABLE wildfire AS SELECT * FROM 'wildfire_data.parquet';")
+
+    # Start  DuckDB HTTP server
+    os.environ['DUCKDB_HTTPSERVER_FOREGROUND'] = '1'
+    info = con.execute("SELECT httpserve_start('0.0.0.0', 9999, '');").fetchall()
+
+    print("🟢 DuckDB HTTP server is running at http://localhost:9999")
+    print(info)
+
+if __name__ == "__main__":
+    main()
+
